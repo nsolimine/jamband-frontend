a594679ecd799c6221916abf97c15706f397e518
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-03-22 13:11:05 -0600

    started writing my own stuff

---
+++ src/components/playerslist.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="playerslist">
+    <ul>
+      <li>{{players.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'playerslist',
+  data () {
+    return {
+      players: []
+    }
+  },
+  mounted () {
+    const apiURL = 'https://jambandbackend.herokuapp.com/players'
+    fetch(apiURL)
+      .then(response => response.json())
+      .then(players => {
+        console.log(players)
+        this.players = players
+      })
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
