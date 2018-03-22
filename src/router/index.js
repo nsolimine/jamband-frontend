ac6d3abcfcc2866650c4009fbf9d3482add09d57
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-03-22 15:22:18 -0600

    players show up now

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import playerslist from '@/components/playerslist'
+import Playerslist from '@/components/playerslist'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'playerslist',
-      component: playerslist
+      name: 'Playerslist',
+      component: Playerslist
     }
   ]
 })
