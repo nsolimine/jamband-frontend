dc4985e07d01e7f8c597cc7fa01a9f6d66ba98a5
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-03-27 13:50:44 -0600

    singled out individual players as singles

--- src/components/session.vue
+++ src/components/session.vue
@@ -1,14 +1,23 @@
 <template id="session">
-  <ul class="sessionDiv">
-    <h2>Groups</h2>
-    <li v-for="group in session" :key="group.id">
-      <div class="sessionCard">
-        <p>{{group.title}}</p>
-        <p>{{group.date}}</p>
-        <p>{{group.time}}</p>
-      </div>
-    </li>
-  </ul>
+  <div class="sessionDiv">
+    <ul>
+      <h2>Groups</h2>
+      <li v-for="group in res" :key="group.id">
+        <div class="sessionCard">
+          <p>{{group.title}}</p>
+          <p>{{group.date}}</p>
+          <p>{{group.time}}</p>
+        </div>
+        <ul>
+          <h5>Players in this group</h5>
+          <li v-for="player in players" :key="player.id">
+            <div class="playerList">
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -16,27 +25,34 @@ export default {
   name: 'Session',
   data () {
     return {
-      session: [],
-      joined: []
+      res: [],
+      players: [],
+      single: []
     }
   },
   mounted () {
     const sessionURL = 'https://jambandbackend.herokuapp.com/' + 'session'
     fetch(sessionURL)
       .then(response => response.json())
-      .then(session => {
-        console.log(session)
-        this.session = session.session.map(session => {
-          return session
+      .then(res => {
+        this.res = res.res.map(res => {
+          console.log(res)
+          return res
         })
       })
-    return fetch('https://jambandbackend.herokuapp.com/')
-      .then(response => response.json())
-      .then(joined => {
-        console.log(joined)
-        this.joined = joined.map(joined => {
-          return joined
+      .then(players => {
+        this.players = this.res.map(players => {
+          players = players.players
+          console.log(players)
+          return players
         })
+      })
+      .then(single => {
+        for (var i = 0; i < this.players.length; i++) {
+          this.players[i].map(single => {
+            return single
+          })
+        }
       })
   }
 }
@@ -55,7 +71,7 @@ p {
 
 .sessionCard {
   border: 1px solid black;
-  padding: 1rem;
+  padding: 1rem 1rem 0 1rem;
   margin-bottom: 1rem;
 
 }
